b97758df40b692eefaac3d986ea572fe85404909
Abby <abigailsy1@yahoo.com>
2019-03-02 15:25:27 -0800

    Modified templates to display posts from db

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'd_lu2uhmjm4^u=rj7=#ipq&&b7*uzqe6%!u@*#yloq=kvf-v$p'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 # CHECK YOUR IP ADDRESS LATER/TEST IT OUT
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com','10.103.170.195']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition

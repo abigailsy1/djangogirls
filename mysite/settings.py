fb1bde6ab73030904340b5e6e1bed4a631d1683f
Abby <abigailsy1@yahoo.com>
2019-03-02 16:07:40 -0800

    changed some settings in mysite

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'd_lu2uhmjm4^u=rj7=#ipq&&b7*uzqe6%!u@*#yloq=kvf-v$p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 # CHECK YOUR IP ADDRESS LATER/TEST IT OUT/Add ip at the end
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
